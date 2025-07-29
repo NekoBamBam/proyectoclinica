88dc9e0d0e8aeba4e6a1f7d0df4266e31f9bd440
NekoBamBam <coriannicolas21@gmail.com>
2025-07-29 19:06:59 -0300

    html, fotohome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import fondo from "../assets/fondoclinica.png";
+import fondo2 from "../assets/fondomedico.png";
 
 function Home() {
   return (
-    <div className="relative w-full h-96">
+    <div className="relative w-full h-screen">
       {/* Imagen */}
       <img src={fondo} alt="Fondo" className="w-full h-full object-cover" />
 
@@ -10,10 +11,9 @@ function Home() {
       <div className="absolute inset-0 flex items-center justify-center">
         <h1 className="text-white text-3xl font-bold">Bienvenido</h1>
       </div>
+
       <div className="w-full bg-white p-10 min-h-screen flex flex-col items-center justify-center">
-        <h2 className="text-3xl font-bold text-gray-800 mb-4 opacity-0 animate-fade-in">
-          Nuestros Servicios
-        </h2>
+        <img src={fondo2} alt="" className=""/>
         <p className="text-gray-600 text-lg max-w-2xl text-center">
           Ofrecemos atención médica integral, turnos online, especialistas en
           distintas áreas, y mucho más.
